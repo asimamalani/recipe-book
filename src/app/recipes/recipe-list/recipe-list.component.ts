b001cb4a4166712da29c4cdc6f1652d293cb7b20
Asim Amalani <asim.amalani@gmail.com>
2020-03-21 12:29:39 -0400

    test commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -4,7 +4,7 @@ import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
   constructor() {}
@@ -21,7 +21,7 @@ export class RecipeListComponent implements OnInit {
       'Coal fired pizza',
       'Looks Yummy!!!',
       'https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/a019-jakubk-0760-homemade-pizza1.jpg?w=800&dpr=1&fit=default&crop=default&auto=format&fm=pjpg&q=75&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=5d06439b389e718a1d12e8797e55a9f1'
-    )
+    ),
   ];
 
   ngOnInit(): void {}
