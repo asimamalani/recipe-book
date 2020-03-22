52e2118971750f75697c5bd15b12cd3bb16d9b4f
Asim Amalani <asim.amalani@gmail.com>
2020-03-22 18:02:38 -0400

    programmatic navigation to the edit page

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,15 +9,25 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./recipe-detail.component.css'],
 })
 export class RecipeDetailComponent implements OnInit {
-  constructor(private recipeService: RecipeService, private route: ActivatedRoute) {}
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router) {}
 
   @Input() recipe: Recipe;
 
+  id: number;
+
   ngOnInit(): void {
-    this.route.params.subscribe(params => (this.recipe = this.recipeService.getRecipe(+params.id)));
+    this.route.params.subscribe(params => {
+      this.id = +params.id;
+      this.recipe = this.recipeService.getRecipe(this.id);
+    });
   }
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingreditents);
+  }
+
+  onEditRecipe() {
+    // this.router.navigate(['./edit'], { relativeTo: this.route });
+    this.router.navigate(['../', this.id, 'edit'], { relativeTo: this.route });
   }
 }
