065b09e6058a6664d54fe0074a1894816970f704
Asim Amalani <asim.amalani@gmail.com>
2020-03-25 20:05:27 -0400

    add form validations, subscription cleanup in shopping list component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   constructor(private slService: ShoppingListService) {}
+
+  private _ingChangedSub: Subscription;
 
   ingredients: Ingredient[];
 
   ngOnInit(): void {
     this.ingredients = this.slService.getIngredients();
-    this.slService.ingredientsChanged$.subscribe((ingredients: Ingredient[]) => (this.ingredients = ingredients));
+    this._ingChangedSub = this.slService.ingredientsChanged$.subscribe(
+      (ingredients: Ingredient[]) => (this.ingredients = ingredients)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this._ingChangedSub.unsubscribe();
   }
 }
