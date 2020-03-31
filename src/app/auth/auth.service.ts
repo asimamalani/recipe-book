2f0da68fef0ae4395ed8ef352995746e5b3a7fd8
Asim Amalani <asim.amalani@gmail.com>
2020-03-31 14:57:52 -0400

    error handling common scenarios for signup and login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -15,42 +15,42 @@ export class AuthService {
   signup(email: string, password: string) {
     const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`;
     const authRequestBody = { email, password, returnSecureToken: true } as AuthRequestBody;
-    return this.http.post<AuthResponseData>(url, authRequestBody).pipe(
-      catchError(errorResp => {
-        let errorMessage = 'An unknown error has occured!';
-        if (!errorResp.error || !errorResp.error.error) {
-          return throwError(errorMessage);
-        }
-        switch (errorResp.error.error.message) {
-          case 'EMAIL_EXISTS':
-            errorMessage = 'Email already exists';
-            break;
-          default:
-            break;
-        }
-        return throwError(errorMessage);
-      })
-    );
+    return this.http.post<AuthResponseData>(url, authRequestBody).pipe(catchError(this.handleError));
   }
 
   login(email: string, password: string) {
     const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`;
     const authRequestBody = { email, password, returnSecureToken: true } as AuthRequestBody;
-    return this.http.post<AuthResponseData>(url, authRequestBody).pipe(
-      catchError(errorResp => {
-        let errorMessage = 'An unknown error has occured!';
-        if (!errorResp.error || !errorResp.error.error) {
-          return throwError(errorMessage);
-        }
-        switch (errorResp.error.error.message) {
-          case 'EMAIL_EXISTS':
-            errorMessage = 'Email already exists';
-            break;
-          default:
-            break;
-        }
-        return throwError(errorMessage);
-      })
-    );
+    return this.http.post<AuthResponseData>(url, authRequestBody).pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorResp: any) {
+    let errorMessage = 'An unknown error has occured!';
+    if (!errorResp || !errorResp.error || !errorResp.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorResp.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'Email already exists';
+        break;
+      case 'OPERATION_NOT_ALLOWED':
+        errorMessage = 'Operation not allowed';
+        break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+        errorMessage = 'Too many attempts. Please try again later';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'Email not found';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'Password is invalid';
+        break;
+      case 'USER_DISABLED':
+        errorMessage = 'User is disabled';
+        break;
+      default:
+        break;
+    }
+    return throwError(errorMessage);
   }
 }
